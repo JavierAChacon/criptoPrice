5f6ae3f11c9e898e23530fc492a97fb01c54d64a
Alejandro Chacón <alejandro.chr@outlook.es>
2023-05-26 14:49:30 -0400

    setting up the projec

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  font-family: "Lato", Sans Serif;
+  color: #FFF;
+`
+
+function App() {
+  return(
+    <Heading>Desde App</Heading>
+  );
+}
+
+export default App;
