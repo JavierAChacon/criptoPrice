463c4dfbe510f4bf1350a9ecc91ec12e03d638fd
Alejandro Chacón <alejandro.chr@outlook.es>
2023-05-28 00:29:42 -0400

    added a label with styles and personalized hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
 import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png"
+import Formulario from "./components/Formulario";
 
 const Heading = styled.h1`
   font-family: "Lato", Sans Serif;
   color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+  &::after{
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66A2ef;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  margin: 100px auto 0 auto;
+  display: block;
 `
 
 function App() {
   return(
-    <Heading>Desde App</Heading>
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagenes cripto"></Imagen>
+      <div>
+         <Heading>Cotiza criptomonedas al instante</Heading>
+         <Formulario/>
+      </div>
+     
+
+
+    </Contenedor>
   );
 }
 
